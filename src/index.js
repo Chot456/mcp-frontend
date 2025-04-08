dcec6bf420ac0172e5cceb76e6102ab7973e422f
Joseph Diza <sephdiza@gmail.com>
2025-04-08 18:03:43 +0800

    add routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from "react-router";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Tracker from './pages/Tracker/Tracker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<App />} />
+      <Route path="tracker" element={<Tracker />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
